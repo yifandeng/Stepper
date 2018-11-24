8707a1d53458277108c83967f26b60427e454f48
yifandeng <34412379+yifandeng@users.noreply.github.com>
2018-11-24 15:08:07 +0800

    Add files via upload

--- stepper.ino
+++ stepper.ino
@@ -1,6 +1,6 @@
 #include <Stepper.h>
 
-// 这里设置步进电机旋转一圈是多少步
+// 这里设置步进电机旋转一圈是多少步 Set the steps to rotate the stepper motor?
 #define STEPS 200
 
 Stepper stepper(STEPS, 8, 9, 10, 11);
@@ -68,7 +68,7 @@ void loop()
         v1 = d;
     }
 
-    // 判断游戏状态
+    // 判断游戏状态 Judge state
     if(gameState == false && v1 != 0)
     {
         gameState = true;
@@ -88,17 +88,17 @@ void loop()
         stepper.step(total);
         endGame();
     }
-        // 游戏状态为真时开始后续流程
+        // 游戏状态为真时开始后续流程 state=true, start the follow-up process
     if(gameState == true)
     {
         tempPause = true;
-            // 设定电机上升的上限和下限
+            // 设定电机上升的上限和下限 Set the upper and lower limits of the motor rise
         if(total < 11800 && total > 500)
         {
             if(d <= 100)
-            {   // 移动前先检测移动后是否超出安全界限 
+            {   // 移动前先检测移动后是否超出安全界限 Check if the safety limit is exceeded after moving
                 if((total - absval * 30) >= 0 && (total + absval * 90) <= 11800)
-                {   // 判断前一次的手部距离及上升下降状态
+                {   // 判断前一次的手部距离及上升下降状态 Determine the previous hand distance and ascending and descending status
                     if(prev > v1 && prev != 0)
                     {  
                         stepper.step(absval * 30);
@@ -126,7 +126,7 @@ void loop()
             {
                 absval = -absval;
             }
-            // 过滤轻微抖动带来的影响
+            // 过滤轻微抖动带来的影响 Filter the effects of slight jitter
             if(absval >= 3)
             {
                 prev = v1;
